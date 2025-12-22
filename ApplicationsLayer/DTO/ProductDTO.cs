7e9672370a75ad46cf225a2f749c7ebb664b7d6b
Salah Eddin Salam <144003465+SalahSalam@users.noreply.github.com>
2025-12-22 10:14:58 +0100

    DTo

--- ApplicationsLayer/DTO/ProductDTO.cs
+++ ApplicationsLayer/DTO/ProductDTO.cs
@@ -9,7 +9,7 @@ namespace ApplicationsLayer.DTO
     public class ProductDTO
     {
         public int Productid { get; set; }
-        public string? Name { get; set; } = "";
+        public string Name { get; set; } = "";
         public string? Description { get; set; }
         public string? Category { get; set; }
         public decimal Price { get; set; }
